e0774edf1809164f956b1380e58cec6b0fcd59e2
Michael McClimon <mjmcclim@indiana.edu>
2013-08-12 10:44:35 -0400

    Nesting tags for staff elements (mensurations).

--- tml_token_parser/mensuration_parser.rb
+++ tml_token_parser/mensuration_parser.rb
@@ -17,21 +17,45 @@ module TmlTokenParser
     # as comments
     def parse
       args = {}
+      xml_id = ''
 
       err = catch :unrecognized do
         sign = do_mensuration
-        args = " mensuration sign: #{@token} "
+        args['mensur.sign'] = sign
+        xml_id = "staff_mens_#{sign}"
+        args['xml:id'] = xml_id
         nil
       end
 
-      @builder.send(:comment, args) unless err
-      unrecognized(@token, arr) if err
+      if err
+        unrecognized(@token, err)
+        return
+      end
+
+      @builder.staffDef(args)
+
+      # open a staff element, continue parsing until we're out of tokens
+      # or the next token is another mensuration sign
+      @builder.staff ({'def' => "##{xml_id}"}) {
+        until @parent.tokens_left? == false ||
+            @parent.next_token.class == TmlTokenParser::MensurationParser
+          @parent.parse_next
+        end
+      }
+
     end
 
     private
 
     def do_mensuration
+      matches = @token.match(/^([A-Z]+)/)
+      throw :unrecognized, 'no_match' if matches.nil?
 
+      if @@mensurations.has_key? matches[1]
+        return matches[1]
+      else
+        throw :unrecognized, "no_key"
+      end
     end
 
   end
--- tml_token_parser/parser.rb
+++ tml_token_parser/parser.rb
@@ -3,6 +3,8 @@
 module TmlTokenParser
   class Parser
 
+    attr_reader :next_token
+
     # Params:
     # *+builder+:: a <tt>Nokogiri::XML::Builder</tt> instance
     # *+tokens+:: a parsed set of tokens, returned by a +TmlCodeTokenizer+
@@ -10,13 +12,26 @@ module TmlTokenParser
       @builder = builder
       @tokens = tokens
       @current_index = 0
+      set_next_token()
     end
 
     def parse
       parse_next() until @current_index == @tokens.length
     end
 
+    def tokens_left?
+      @current_index < @tokens.length
+    end
+
     def parse_next
+      t = @next_token
+      set_next_token()
+      t.parse
+    end
+
+    private
+
+    def set_next_token
       token = @tokens[@current_index]
       @current_index += 1
 
@@ -36,7 +51,7 @@ module TmlTokenParser
                  TmlTokenParser::MiscParser.new(@builder, token)
                end
       child.set_parent(self)
-      child.parse
+      @next_token = child
     end
 
 
